1c3f51bea2730a976f64c7c3b131391c47fead0e
leoparin <xiaoi20010208@gmail.com>
2023-01-12 10:23:25 +0800

    add get ticket steps functionality

---
+++ src/main/java/com/leo/electricitysystem/exception/IdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.leo.electricitysystem.exception;/**
+* ClassName:idNotFoundException
+* PackageName:com.leo.electricitysystem.exception
+* Description:
+* @Date 2023/1/11 15:51
+* @Author leo
+**/    public class idNotFoundException {
+}
---
+++ src/test/java/com/leo/electricitysystem/functionality/ErrorTest.java
@@ -0,0 +1,8 @@
+package com.leo.electricitysystem.functionality;/**
+* ClassName:ErrorTest
+* PackageName:com.leo.electricitysystem.functionality
+* Description:
+* @Date 2023/1/11 12:01
+* @Author leo
+**/    public class ErrorTest {
+}
